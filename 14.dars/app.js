5f91052f974ccd41a5f1a7c7fa540ae28d9a1dbb
Ilhom <majidov@gmail.com>
2023-08-03 22:28:21 +0500

    14chi dars berilgan uy ishi

---
+++ 14.dars/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.set("view engine", "ejs"); 
+app.use(express.static("public")); 
+
+app.get("/", (req, res) => {
+    // const messages = ["Hello"];
+    res.render("index"); 
+});
+
+
+
+let users = [];
+
+io.on("connection", (socket) => {
+    // console.log("A user connected");
+    users.push({id: socket.id, name: `user ` + users.length}); 
+    const currentUser = users.find((user) => user.id === socket.id)
+
+
+    socket.on("all", ({message}) => {
+        io.emit("response", { message, user: currentUser.name }); 
+    });
+    socket.on("me", ({message, to}) => {
+        io.to(to).emit("response", { message, user: currentUser.name }); 
+    });
+
+    socket.emit("name", { name: currentUser.name });
+    socket.emit("users", {users})
+
+    socket.on("disconnect", function () {
+        const index = users.findIndex((user) => user.id === socket.id);
+        if (index !== -1) {
+            users.splice(index, 1);
+        }
+    })
+});
+
+http.listen(4000, () => {
+    console.log("Server is listening on port 4000");
+});
